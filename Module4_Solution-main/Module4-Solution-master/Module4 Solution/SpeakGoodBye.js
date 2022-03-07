100fd691a72967aed08acdd7a4032de7a072f102
vamsi540 <vamsibhargav4404@gmail.com>
2022-03-07 21:18:34 +0530

    updated

---
+++ Module4_Solution-main/Module4-Solution-master/Module4 Solution/SpeakGoodBye.js
@@ -0,0 +1,7 @@
+var byeSpeaker={};
+byeSpeaker.speakWord = "Good Bye";
+
+byeSpeaker.speak=function (name) {
+  console.log(byeSpeaker.speakWord + " " + name);
+}
+
---
+++ Module4_Solution-main/Module4-Solution-master/Module4 Solution/SpeakHello.js
@@ -0,0 +1,7 @@
+var helloSpeaker={};
+helloSpeaker.speakWord = "Hello";
+
+helloSpeaker.speak=function (name) {
+  console.log(helloSpeaker.speakWord + " " + name);
+}
+
